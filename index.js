26b610da3b7adadfd95a45a0cd3200906f1ab1e1
Spencer Foster <fosterjs@me.com>
2017-11-18 16:42:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'eggplant emoji';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'poop emoji'
+}
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'eggplant emoji'
+  let favoriteCustomer = 'poop emoji'
+}
